f6ac64ddf611a1ffa920a475188b768a7db0890e
Nobody <Nobody@nowhere.to.go>
2015-06-05 23:20:34 -0400

    Bug fixes in input file making

--- XcIO/write_egs_input.py
+++ XcIO/write_egs_input.py
@@ -7,18 +7,19 @@ def write_input(template, file_prefix, coll, shot):
     Write out egs input file from template
     """
     
+    lines = []
     with open(template, "rt") as f:
         lines = f.readlines()
         
     # alter title
     qname = names_helper.make_qualified_name(file_prefix, coll, shot)
         
-    lines[0] = qname + ";" + "SPAD=18cm;SAD=36cm" + "                #!GUI1.0"
+    lines[0] = qname + ";" + "SPAD=18cm;SAD=36cm" + "                                             #!GUI1.0\n"
                
     # alter phase space file name
-    lines[2] = qname + names_helper.EGSPHSF_EXT
+    lines[2] = qname + names_helper.EGSPHAN_EXT + "\n"
     
-    lines[44] = str(coll) + names_helper.EGSPHSF_EXT
+    lines[43] = str(coll) + names_helper.EGSPHSF_EXT + "\n"
     
     fname = names_helper.make_egsinput_name(file_prefix, coll, shot)
     with open(fname, "wt") as f:
