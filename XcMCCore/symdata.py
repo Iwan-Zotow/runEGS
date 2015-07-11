fa7a849c7441444e99035b20fe1e345e18bb1067
Nobody <nobody@nowhere.to.go>
2015-07-10 23:29:40 -0400

    Added KDD style symmetric data class

---
+++ XcMCCore/symdata.py
@@ -0,0 +1,155 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import logging
+
+import phdata
+
+class symdata(phdata.phdata):
+    """
+    phantom data which contains float-per-voxel together with dimensions,
+    plus symmetrization flags and logic
+    """
+    
+    # STD - standard data
+    # SYM - symmetrized data
+    STD, SYM = range(2)
+    
+    EPS = 0.01 # in mm
+    
+    def __init__(self, bx, by, bz):
+        """
+        Sym data constructor
+        
+        Parameters
+        ----------
+        
+        bx: array of floats
+            voxel boundaries in X, mm
+            
+        by: array of floats
+            voxel boundaries in Y, mm
+        
+        bz: array of floats
+            voxel boundaries in Z, mm       
+        """
+
+        super(symdata, self).__init__(bx, by, bz)
+        
+        self._sym_x = STD
+        self._sym_y = STD
+        self._sym_z = STD
+        
+        logging.info("symdata object constructed")
+                        
+    def data(self):
+        """
+        Returns phantom data
+        """
+        return self._data
+
+    def sym_x(self):
+        """
+        Returns X symmetry flag
+        """
+        return self._sym_x
+
+    def sym_y(self):
+        """
+        Returns Y symmetry flag
+        """
+        return self._sym_y
+
+    def sym_z(self):
+        """
+        Returns Z symmetry flag
+        """
+        return self._sym_z
+        
+    @staticmethod
+    def could_sym(bounds, eps):
+        """
+        Check if bounds are symmetric so 
+        we could average by just summation
+        :param bounds: array of bounds, n+1 in size
+        :param eps: float, epsilon
+        """
+        
+        n = len(bounds) - 1 # number of voxels
+        for i in range(1, n):
+            ileft_l = i - 1
+            ileft_r = ileft_l + 1
+        
+            irght_l = n - i
+            irght_r = irght_l + 1
+        
+            dleft = bounds[ileft_r] - bounds[ileft_l]
+            drght = bounds[irght_r] - bounds[irght_l]
+        
+            delta = math.fabs(drght - dleft)
+            if (delta > eps):
+                return False
+
+        # bins are equal within eps
+        return True
+        
+    def could_sym_x(self):
+        """
+        True if could symmetrize X, False otherwise
+        """
+        return symdata.could_sym(self._bx, EPS)
+
+    def could_sym_y(self):
+        """
+        True if could symmetrize Y, False otherwise
+        """
+        return symdata.could_sym(self._by, EPS)
+
+    def could_sym_z(self):
+        """
+        True if could symmetrize Z, False otherwise
+        """
+        return symdata.could_sym(self._bz, EPS)
+
+    def do_sym_x(self):
+        """
+        Make data symmetric over X
+        """
+        
+        nx = self.nx()
+        ny = self.ny()
+        nz = self.nz()
+        
+        scale = np.float32(0.5)
+        data = np.empty((nx, ny, nz), dtype=np.float32)
+    
+        for iz in range(0, nz):
+            for iy in range(0, ny):
+                for ix in range(0, nx):
+                    dleft = self._data[ix,iy,iz]
+                    drght = self._data[nx-1-ix,iy,iz]
+                    data[ix,iy,iz] = (dleft + drght) * scale
+    
+        self._data = data
+        
+    def do_sym_y(self):
+        """
+        Make data symmetric over Y
+        """
+        
+        nx = self.nx()
+        ny = self.ny()
+        nz = self.nz()
+        
+        scale = np.float32(0.5)
+        data = np.empty((nx, ny, nz), dtype=np.float32)
+    
+        for iz in range(0, nz):
+            for iy in range(0, ny):
+                for ix in range(0, nx):
+                    dleft = self._data[ix,iy,iz]
+                    drght = self._data[ix,ny-1-iy,iz]
+                    data[ix,iy,iz] = (dleft + drght) * scale
+    
+        self._data = data
+
