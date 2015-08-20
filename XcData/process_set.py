2c45a8eff26ec869d3a6e59c0bc44e3a0ada84b8
Nobody <nobody@nowhere.to.go>
2015-08-20 16:31:41 -0400

    Process sets of cups based on the tag

--- XcData/process_set.py
+++ XcData/process_set.py
@@ -6,11 +6,24 @@ import process_cup
 def process_set(set_dir, out_dir, zshift):
     """
     """
-    for cup_dir in os.listdir(set_dir):
-        if fnmatch.fnmatch(cup_dir, "R8O*C??"):
-        
-            dname = os.path.join(set_dir, cup_dir)
-            process_cup.process_cup(dname, out_dir, zshift)
+    
+    tag = "R8O1IS"
+    
+    for k in range(1,10):
+        cup_tag = tag + "0" + str(k)
+        process_cup.process_cup(set_dir, cup_tag, out_dir, zshift)        
+
+    tag = "R8O2IM"
+    
+    for k in range(1,10):
+        cup_tag = tag + "0" + str(k)
+        process_cup.process_cup(set_dir, cup_tag, out_dir, zshift)
+            
+    tag = "R8O2IM"
+    
+    for k in range(1,10):
+        cup_tag = tag + "0" + str(k)
+            process_cup.process_cup(set_dir, cup_tag, out_dir, zshift)
 
 if __name__ == "__main__":
-    process_set("/home/sphinx/gcloud", "qqq", -140.0)
+    process_set("/home/sphinx/gcloud", "qqq", 140.0)
