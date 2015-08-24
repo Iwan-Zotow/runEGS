cad77aabcad1683b831714dd5ea895290e3e9217
Nobody <nobody@nowhere.to.go>
2015-08-24 16:14:27 -0400

    Added explicit request for Sym Y

--- XcData/main.py
+++ XcData/main.py
@@ -4,7 +4,10 @@
 import process_set
 
 if __name__ == "__main__":
-    process_set.process_set("/home/sphinx/gcloud", "R8O1IS", 1, 9,  "OutYZ", 116.0, True)
-    process_set.process_set("/home/sphinx/gcloud", "R8O2IM", 1, 10, "OutYZ", 140.0, True)
-    process_set.process_set("/home/sphinx/gcloud", "R8O3IL", 1, 9,  "OutYZ", 153.0, True)
-    process_set.process_set("/home/sphinx/gcloud", "R8O0IQ", 0, 0,  "OutYZ", 15.0, True)
+    # set it to False if you want the default behaviour
+    symmetrizeY = True
+
+    process_set.process_set("/home/sphinx/gcloud", "R8O1IS", 1, 9,  "OutYZ", 116.0, symmetrizeY)
+    process_set.process_set("/home/sphinx/gcloud", "R8O2IM", 1, 10, "OutYZ", 140.0, symmetrizeY)
+    process_set.process_set("/home/sphinx/gcloud", "R8O3IL", 1, 9,  "OutYZ", 153.0, symmetrizeY)
+    process_set.process_set("/home/sphinx/gcloud", "R8O0IQ", 0, 0,  "OutYZ",  15.0, symmetrizeY)
