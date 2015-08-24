d32ab62663be0d65def8c4f53f9aeef6419e501d
Nobody <nobody@nowhere.to.go>
2015-08-23 23:50:05 -0400

    Main driver for conversion

---
+++ XcData/main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import process_set
+
+if __name__ == "__main__":
+    process_set.process_set("/home/sphinx/gcloud", "R8O1IS", 1, 9,  "OutYZ", 116.0, True)
+    process_set.process_set("/home/sphinx/gcloud", "R8O2IM", 1, 10, "OutYZ", 140.0, True)
+    process_set.process_set("/home/sphinx/gcloud", "R8O3IL", 1, 9,  "OutYZ", 153.0, True)
+    process_set.process_set("/home/sphinx/gcloud", "R8O0IQ", 0, 0,  "OutYZ", 15.0, True)
