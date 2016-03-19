e6223a71cf606fdb9de24f2978cedf0f6107d801
Nobody <nobody@nowhere.to.go>
2016-03-18 22:55:26 -0400

    Copy ICP/OCP parameters  from storage to working directory

--- XcIO/cup_downloader.py
+++ XcIO/cup_downloader.py
@@ -14,12 +14,12 @@ class cup_downloader(object):
         logging.debug(host_ip)
  data
     """
-    
+
     def __init__(self, host_ip, host_dir, cup_dir, file_prefix, user_id, user_pass):
         """
         Constructor
         """
-        
+
         logging.info("Start cup downloader construction")
         logging.debug(host_ip)
         logging.debug(host_dir)
@@ -27,44 +27,44 @@ class cup_downloader(object):
         logging.debug(file_prefix)
         logging.debug(user_id)
         logging.debug(user_pass)
-        
+
         self._host_ip  = host_ip
         self._host_dir = host_dir
         self._cup_dir  = cup_dir
-        
+
         self._file_prefix = file_prefix
         self._user_id     = user_id
         self._user_pass   = user_pass
-        
+
         self._rc = 0
-        
+
         logging.info("Done cup downloader construction")
-        
+
     def single_load_ssh(self, src):
         """
         Load single cup using SSH protocol
         """
         try:
             logging.info("Start single cup download: {0}".format(src))
-            
+
             rc = subprocess.call(["sshpass", "-p", self._user_pass, "scp", self._user_id +"@" + self._host_ip + ":" + src, self._cup_dir],
                                  stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         except OSError:
             logging.debug("single_load_ssh: OS failure")
             rc = -1
             return rc
-            
+
         logging.info("One cup loaded")
-            
+
         return rc
-        
+
     def single_load_ftp(self, src):
         """
         Load single cup using FTP protocol
         """
         try:
             logging.info("Start single cup download: {0}".format(src))
-            
+
             source = "ftp://" + self._user_id + ":" + self._user_pass + "@" + self._host_ip + "/" + src
             cmd = ["wget", "-r", "-nH", "-nd", "-P", self._cup_dir, source]
             rc = subprocess.call(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -72,11 +72,11 @@ class cup_downloader(object):
             logging.debug("single_load_ftp: OS failure")
             rc = -1
             return rc
-            
+
         logging.info("One cup loaded")
-            
+
         return rc
-                
+
     def single_load_copy(self, src):
         """
         Load single cup using fs copy
@@ -87,13 +87,13 @@ class cup_downloader(object):
 
         try:
             logging.info("Start single cup download: {0}".format(src))
-            
+
             dirOfFile, nameOfFile = os.path.split(src)
 
             source = os.path.join(os.getcwd(),up, cups, nameOfFile)
-            
+
             shutil.copy(source, self._cup_dir)
-            
+
             rc = 0
 
         except OSError:
@@ -117,33 +117,33 @@ class cup_downloader(object):
         """
         Load all cups info from server
         """
-        
+
         logging.info("Start cup downloading")
-        
+
         self._rc = 0
-        
+
         # form the command line
-        
+
         fname = self._file_prefix + "_" + "KddCurveA.txt"
         src = os.path.join( self._host_dir, fname )
         rc = self.single_load(src)
-        
+
         if rc != 0:
             self._rc = rc
             return
-            
+
         fname = self._file_prefix + "_" + "KddCurveB.txt"
         src = os.path.join( self._host_dir, fname )
         rc = self.single_load(src)
-        
+
         if rc != 0:
             self._rc = rc
             return
-            
+
         fname = self._file_prefix + "_" + "KddCurveC.txt"
         src = os.path.join( self._host_dir, fname )
         rc = self.single_load(src)
-        
+
         if rc != 0:
             self._rc = rc
             return
@@ -151,13 +151,21 @@ class cup_downloader(object):
         fname = self._file_prefix + ".json"
         src = os.path.join( self._host_dir, fname )
         rc = self.single_load(src)
-        
+
+        fname = self._file_prefix + ".icpparam"
+        src = os.path.join( self._host_dir, fname )
+        rc = self.single_load(src)
+
+        fname = self._file_prefix + ".ocpparam"
+        src = os.path.join( self._host_dir, fname )
+        rc = self.single_load(src)
+
         self._rc = rc
-        logging.info("Done cup downloading")        
-        
+        logging.info("Done cup downloading")
+
     def rc(self):
         """
         Returns return code of the downloud operation
         """
-        
+
         return self._rc
