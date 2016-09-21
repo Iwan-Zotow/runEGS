1c2e75d08ed5b946927a3b31013927076066f798
Nobody <nobody@nowhere.to.go>
2016-09-20 22:27:53 -0400

    Make flexible phsf input

--- XcIO/run_dosxyz.py
+++ XcIO/run_dosxyz.py
@@ -13,22 +13,22 @@ def path2dosxyznrc():
     """
 
     egs_path = os.environ["EGS_HOME"]
-    
+
     if egs_path == None:
         raise RuntimeError("path2dosxyznrc", "No EGS_HOME evn.variable")
-        
+
     return os.path.join(egs_path, DXYZ)
-    
+
 def move_results(wrk_dir, fname):
     """
     Move results from dosxyz dir to work dir
     """
-    
+
     # just in case, take file name
     head, name = os.path.split(fname)
-    
+
     src = os.path.join(path2dosxyznrc(), name)
-    dst = fname    
+    dst = fname
 
     os.rename(src, dst)
 
@@ -36,23 +36,23 @@ def run_dosxyz(wrk_dir, egs_inp, pegs_inp):
     """
     run dosxyz with a given egs and pegs input files
     """
-    
+
     logging.info("Running DOSXYZ")
     logging.debug(wrk_dir)
     logging.debug(egs_inp)
     logging.debug(pegs_inp)
-    
+
     start = time.time()
-    
+
     process_name = DXYZ
-        
+
     src = os.path.join(wrk_dir, egs_inp)
     lnk = os.path.join(path2dosxyznrc(), os.path.basename(egs_inp))
-    
+
     if os.path.isfile(lnk):
         os.unlink(lnk)
-        
-    os.symlink(src, lnk)    
+
+    os.symlink(src, lnk)
 
     rc = subprocess.call([process_name, "-i", os.path.basename(egs_inp), "-p", pegs_inp, "-b"],
                           stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -60,23 +60,22 @@ def run_dosxyz(wrk_dir, egs_inp, pegs_inp):
         logging.info("Cannot run DOSXYZ")
         logging.debug(rc)
         return rc
-                          
+
     thetime = time.time() - start
-    
+
     if os.path.isfile(lnk):
         os.unlink(lnk)
 
     logging.info("Finished with running DOSXYZ: {0}".format(thetime))
-    
+
     # move the data to working folder
     name,ext = os.path.splitext(egs_inp)
-    
+
     move_results(wrk_dir, name + ".3ddose")
     move_results(wrk_dir, name + ".errors")
     move_results(wrk_dir, name + ".egslog")
     move_results(wrk_dir, name + ".egslst")
-                          
-    logging.info("Done with DOSXYZ")
-    
-    return rc
 
+    logging.info("Done with DOSXYZ")
+
+    return rc
--- XcIO/write_egs_input.py
+++ XcIO/write_egs_input.py
@@ -6,6 +6,17 @@ import logging
 from XcMath import conversion
 from XcIO   import names_helper
 
+def find_egsphsp1(lines):
+    """
+    """
+    k = 0
+    for l in lines:
+        if names_helper.EGSPHSF_EXT in l:
+            return k
+        k += 1
+
+    return -1
+
 def write_input(wrk_dir, template, full_prefix, cl, shot):
     """
     Write EGS input file from template
@@ -39,9 +50,13 @@ def write_input(wrk_dir, template, full_prefix, cl, shot):
     with open(template, "rt") as f:
         lines = f.readlines()
 
+    phsf_pos = find_egsphsp1(lines)
+    if phsf_pos < 0:
+        raise RuntimeError("no egsphsp1 position in template")
+
     lines[0] = full_prefix + ";" + "SPAD=18cm;SAD=36cm" + "                                             #!GUI1.0\n"
 
-    # alter phase space file name
+    # alter EGS phantom file name
     lines[2] = os.path.join(wrk_dir, full_prefix + names_helper.EGSPHAN_EXT + "\n")
 
     # making source input line with isocenter
@@ -53,7 +68,8 @@ def write_input(wrk_dir, template, full_prefix, cl, shot):
     # replace iso center with user defined shot, in cm
     lines[5] = fmt.format(src[0], src[1], conversion.mm2cm(X), conversion.mm2cm(Y), conversion.mm2cm(Z), src[5], src[6], src[7], src[8], src[9], src[10], src[11], src[12])
 
-    lines[43] = str(cl) + names_helper.EGSPHSF_EXT + "\n"
+    # making phsf file input
+    lines[phsf_pos] = str(cl) + names_helper.EGSPHSF_EXT + "\n"
 
     fname = os.path.join(wrk_dir, names_helper.make_egsinput_name(full_prefix))
     with open(fname, "wt") as f:
@@ -63,4 +79,3 @@ def write_input(wrk_dir, template, full_prefix, cl, shot):
     logging.debug(fname)
 
     return fname
-
