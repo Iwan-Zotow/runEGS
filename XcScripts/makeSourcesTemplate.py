464d18b83050f00337527ef84169f2375ee9a19b
Nobody <nobody@nowhere.to.go>
2017-09-01 12:12:40 -0400

    25 sources templates and code generator

--- XcScripts/makeSourcesTemplate.py
+++ XcScripts/makeSourcesTemplate.py
@@ -5,7 +5,8 @@
 import os
 import sys
 
-polar_angles = [18, 24, 29, 35, 41, 47, 53]
+POLAR_ANGLES_START = 18
+POLAR_ANGLES_TOTAL = 36
 
 def main(nof_sources):
     """
@@ -15,20 +16,19 @@ def main(nof_sources):
         Number of sources
     """
 
-    if nof_sources > len(polar_angles):
+    if nof_sources > POLAR_ANGLES_TOTAL:
         raise ValueError("N of sources is too large")
 
     if nof_sources < 1:
         raise ValueError("N of sources is too small, shall be positive")
 
-    first = 90 - polar_angles[0]
-    last  = 90 - polar_angles[nof_sources-1]
-    ns    = first - (last - 1)
+    first = 90 - POLAR_ANGLES_START
+    last  = 90 - (nof_sources + POLAR_ANGLES_START)
 
-    print("2, 8, 0, 0, 0, -{}, 18, 180, 0, 180, 0, 0, 0".format(ns))
-    delta_phi = 1.0 / float(ns)
+    print("2, 8, 0, 0, 0, -{}, 18, 180, 0, 180, 0, 0, 0".format(nof_sources))
+    delta_phi = 1.0 / float(nof_sources)
     k = 0
-    for a in range(first, last-1, -1):
+    for a in range(first, last, -1):
         phi_start = 0.0 + delta_phi*float(k)
         phi_end   = 359.0 + delta_phi*float(k)
         s = "0, {0}, {1:8.6}, {2:.9}, 360, 1".format(a, phi_start, phi_end)
@@ -36,5 +36,5 @@ def main(nof_sources):
         k += 1
 
 if __name__ == "__main__":
-    main(7)
+    main(25)
     sys.exit(0)
